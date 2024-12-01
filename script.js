f361dd3b9becc509dbd91b127533ef20f9f031d8
AndelKaz <149527003+AndelKaz@users.noreply.github.com>
2024-12-01 22:07:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// <--------TASK_1---------->
+// let num = 0;
+// while (num <10) {
+//   num= num + 1
+//   console.log(num);
+// }
+
+// <--------TASK_2----------> (Я тут не зрозумів завдання тому зробив 2 кода - щоб виводило тільки парні і не парні)
+
+
+// for(let i = 2; i <=20; i +=1) {
+
+//     if(i % 2 !== 0) {
+
+//         continue;
+//     }
+//     console.log( i);
+// }
+
+
+// for(let i = 2; i <=20; i +=1) {
+
+//     if(i % 2 === 0) {
+
+//         continue;
+//     }
+//     console.log( i);
+// }
+
+
+// <--------TASK_3---------->.
+
+
+// for(let i = 1; i <=10; i += 1 ){
+//     let num = i * 7;
+//     if(i === 7){
+//         break
+//     }
+//     console.log(num)
+// }
+
+// <--------TASK_4---------->.
+
+// let n = 16;
+// for(let i = 0; i <n; i +=1){
+//     if(i >= n){
+//         break
+//     }
+//     console.log(i)
+// }
+
+// <--------TASK_5---------->.
+
+// for(let i = 1; i <=20; i +=1){
+//     if(i % 3 === 0){
+//         continue
+//     }
+//     console.log(i);
+// }
